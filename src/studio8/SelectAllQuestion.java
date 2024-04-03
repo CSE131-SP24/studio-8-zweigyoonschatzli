370b20c957b7ff22b1c2e6ec8bf06a7c8d41a49d
meredith <meredith@localhost>
2024-04-03 15:52:26 -0500

    studio 8

--- src/studio8/SelectAllQuestion.java
+++ src/studio8/SelectAllQuestion.java
@@ -1,15 +1,18 @@
 package studio8;
 
 public class SelectAllQuestion extends MultipleChoiceQuestion {
-
 	public SelectAllQuestion(String prompt, String answer, String[] choices) {
 		//Hint: 1 point per choice
 		//FIXME
+		super(prompt, answer, choices.length, choices); 
+		
 	}
 	
 	public int checkAnswer(String givenAnswer) {
 		//FIXME Should return partial credit (if earned)!
-		return 0;
+		int MCA = findMissingCorrectAnswers(givenAnswer);
+		int ICA = findIncorrectGivenAnswers (givenAnswer);
+		return  super.getPoints() - (MCA + ICA);
 	}
 
 	private int findMissingCorrectAnswers(String givenAnswer) {
